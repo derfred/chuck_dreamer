a1125e1489b17623c956e6effe571e83ee619939
Frederik Fix <ich@derfred.com>
2026-05-21 08:29:41 +0200

    augmentation workflow

--- src/chuck_dreamer/real/object_localization/cli/__init__.py
+++ src/chuck_dreamer/real/object_localization/cli/__init__.py
@@ -1,5 +1,6 @@
 """Click entry points for the object-localization pipeline."""
 from .annotate_mat import annotate_mat_cmd
+from .augment_keyframes import augment_keyframes_cmd
 from .calibrate_intrinsics import calibrate_intrinsics_cmd
 from .prompt_episodes import prompt_episodes_cmd
 from .test_calibration import test_calibration_cmd
@@ -7,6 +8,7 @@ from .test_object_pose import test_object_pose_cmd
 
 __all__ = [
   "annotate_mat_cmd",
+  "augment_keyframes_cmd",
   "calibrate_intrinsics_cmd",
   "prompt_episodes_cmd",
   "test_calibration_cmd",
